f3d9227f75729994e66d661b3634cb74c7ac16ca
DennisJoelLimaAlvarez <darkbardok123@gmail.com>
2021-12-03 10:26:22 -0400

    examen spa realizado

---
+++ src/Header.js
@@ -0,0 +1,9 @@
+export const Header = () => {
+    return (
+        <header className="App-header">
+        <Link to="/home">Home</Link>
+        <Link to="/about">About</Link>
+        <Link to="/dashboard">Dashboard</Link>
+      </header>
+    );
+};
